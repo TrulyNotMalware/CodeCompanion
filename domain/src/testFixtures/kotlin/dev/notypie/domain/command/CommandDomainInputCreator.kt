aad7875ad34898fbd99e95bd8b6fa53b00a3eff7
TrulyNotMalware <notleebutyee@gmail.com>
2025-08-19 18:27:53 +0900

    test : domain test code

--- domain/src/testFixtures/kotlin/dev/notypie/domain/command/CommandDomainInputCreator.kt
+++ domain/src/testFixtures/kotlin/dev/notypie/domain/command/CommandDomainInputCreator.kt
@@ -1,4 +1,23 @@
 package dev.notypie.domain.command
 
-class CommandDomainInputCreator {
-}
+import dev.notypie.domain.command.dto.CommandBasicInfo
+import java.util.UUID
+
+const val TEST_APP_ID = "A12ABCDEFG" // starts with A
+const val TEST_USER_ID = "U012ABCDEFG" // starts with U
+const val TEST_CHANNEL = "C012ABCDEFG" // starts with C
+const val TEST_TOKEN = "I_AM_TEST_TOKEN"
+
+fun createCommandBasicInfo(
+    appId: String = TEST_APP_ID,
+    appToken: String = TEST_TOKEN,
+    publisherId: String = TEST_USER_ID,
+    channel: String = TEST_CHANNEL,
+    idempotencyKey: UUID = UUID.randomUUID()
+) = CommandBasicInfo(
+    appId = appId,
+    appToken = appToken,
+    publisherId = publisherId,
+    channel = channel,
+    idempotencyKey = idempotencyKey
+)
