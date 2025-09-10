30a6984858beda1a32e14053a02689b37b19e400
TrulyNotMalware <notleebutyee@gmail.com>
2025-09-10 13:10:46 +0900

    build : build library update
    - gradle version update to 9.0.0
    - kotlin version update to 2.2.10
    - kotest version update to 6.0.3
    build tool update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "3.5.3" apply false
+    id("org.springframework.boot") version "3.5.5" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("java-library")
     id("java-test-fixtures")
-    kotlin("jvm") version "2.1.21"
-    kotlin("plugin.spring") version "2.1.21" apply false
-    kotlin("plugin.jpa") version "2.1.21" apply false
+    kotlin("jvm") version "2.2.10"
+    kotlin("plugin.spring") version "2.2.10" apply false
+    kotlin("plugin.jpa") version "2.2.10" apply false
 }
 
 java {
@@ -15,8 +15,7 @@ java {
 }
 
 ext{
-    set("kotestVersion", "5.9.1") // https://kotest.io/docs/changelog.html
-    set("kotestSpringExtensionVersion", "1.3.0") // https://kotest.io/docs/extensions/spring.html
+    set("kotestVersion", "6.0.3") // https://kotest.io/docs/changelog.html
     set("slackSdkVersion", "1.45.3")
     set("mockkVersion", "1.14.5")
 }
@@ -56,19 +55,22 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
 
     dependencies {
+        //Kotest-bom
+        implementation(platform("io.kotest:kotest-bom:${rootProject.extra.get("kotestVersion")}"))
+
         implementation(kotlin("reflect"))
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
         implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
         //Kotlin logging
-        implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
+        implementation("io.github.oshai:kotlin-logging-jvm:7.0.13")
         testFixturesImplementation(kotlin("reflect"))
 
         testImplementation("io.mockk:mockk:${rootProject.extra.get("mockkVersion")}")
-        testImplementation("io.kotest:kotest-runner-junit5-jvm:${rootProject.extra.get("kotestVersion")}")
-        testImplementation("io.kotest.extensions:kotest-extensions-spring:${rootProject.extra.get("kotestSpringExtensionVersion")}")
-        testImplementation("io.kotest:kotest-assertions-core-jvm:${rootProject.extra.get("kotestVersion")}")
+        testImplementation("io.kotest:kotest-runner-junit5")
+        testImplementation("io.kotest:kotest-extensions-spring")
+        testImplementation("io.kotest:kotest-assertions-core")
 
     }
 }
