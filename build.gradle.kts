b76fad4180a23e9b53f8ff78499141ecfec868f9
TrulyNotMalware <notleebutyee@gmail.com>
2025-10-16 16:49:04 +0900

    chore : Springboot & Kotlin version update
    - update documents
    - update kotlin to 2.2.20
    - update springboot to 3.5.6
    - gradle-configs and scripts update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,36 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import org.jlleitschuh.gradle.ktlint.tasks.GenerateReportsTask
 
 plugins {
-    id("org.springframework.boot") version "3.5.5" apply false
+    id("org.springframework.boot") version "3.5.6" apply false
     id("java-library")
     id("java-test-fixtures")
     id("org.jlleitschuh.gradle.ktlint").version("13.1.0")
-    kotlin("jvm") version "2.2.10"
-    kotlin("plugin.spring") version "2.2.10" apply false
-    kotlin("plugin.jpa") version "2.2.10" apply false
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.spring") version "2.2.20" apply false
+    kotlin("plugin.jpa") version "2.2.20" apply false
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+        vendor = JvmVendorSpec.ADOPTIUM
+    }
 }
 
 ext {
     set("kotestVersion", "6.0.3") // https://kotest.io/docs/changelog.html
-    set("slackSdkVersion", "1.45.3")
-    set("mockkVersion", "1.14.5")
-    set("springBootVersion", "3.5.5")
+    set("slackSdkVersion", "1.45.4")
+    set("mockkVersion", "1.14.6")
+    set("springBootVersion", "3.5.6")
     set("jacksonVersion", "2.19.2")
 }
 
 kotlin {
+    jvmToolchain(21)
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
         jvmTarget = JvmTarget.JVM_21
@@ -60,10 +67,33 @@ allprojects {
     tasks.withType<JavaCompile> {
         sourceCompatibility = "21"
         targetCompatibility = "21"
+
+        options.apply {
+            encoding = "UTF-8"
+            compilerArgs.addAll(
+                listOf(
+                    "-parameters",
+                ),
+            )
+        }
+    }
+
+    tasks.withType<KotlinJvmCompile> {
+        compilerOptions {
+            freeCompilerArgs.addAll(
+                "-Xjsr305=strict",
+                "-Xjvm-default=all",
+            )
+        }
     }
 
     tasks.withType<Test> {
         useJUnitPlatform()
+
+        jvmArgs(
+            "-Xmx4g",
+            "-Dfile.encoding=UTF-8",
+        )
     }
 }
 /*
@@ -83,50 +113,22 @@ subprojects {
 
     dependencies {
         // Kotest-bom
-        implementation(
-            platform(
-                "io.kotest:kotest-bom:${rootProject.extra.get(
-                    "kotestVersion",
-                )}",
-            ),
-        )
+        implementation(platform("io.kotest:kotest-bom:${rootProject.extra.get("kotestVersion")}"))
         // Jackson-bom
-        implementation(
-            platform(
-                "com.fasterxml.jackson:jackson-bom:${rootProject.extra.get(
-                    "jacksonVersion",
-                )}",
-            ),
-        )
+        implementation(platform("com.fasterxml.jackson:jackson-bom:${rootProject.extra.get("jacksonVersion")}"))
 
         implementation(kotlin("reflect"))
-        implementation(
-            "com.fasterxml.jackson.module:jackson-module-kotlin",
-        )
-        implementation(
-            "com.fasterxml.jackson.datatype:jackson-datatype-jdk8",
-        )
-        implementation(
-            "com.fasterxml.jackson.datatype:jackson-datatype-jsr310",
-        )
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
+        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
         // Kotlin logging
-        implementation(
-            "io.github.oshai:kotlin-logging-jvm:7.0.13",
-        )
+        implementation("io.github.oshai:kotlin-logging-jvm:7.0.13")
         testFixturesImplementation(kotlin("reflect"))
 
-        testImplementation(
-            "io.mockk:mockk:${rootProject.extra.get(
-                "mockkVersion",
-            )}",
-        )
+        testImplementation("io.mockk:mockk:${rootProject.extra.get("mockkVersion")}")
         testImplementation("io.kotest:kotest-runner-junit5")
-        testImplementation(
-            "io.kotest:kotest-extensions-spring",
-        )
-        testImplementation(
-            "io.kotest:kotest-assertions-core",
-        )
+        testImplementation("io.kotest:kotest-extensions-spring")
+        testImplementation("io.kotest:kotest-assertions-core")
     }
 }
